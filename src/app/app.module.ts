8782c803da6ef5f29c95fb638ddb2f07d7f48bf2
Nathan Brinton <nab.natethegreat@gmail.com>
2022-02-16 01:57:47 -0700

    - Created stub `page-not-found` and `plan` components. - Added FontAwesome. - Added `ng-bootstrap`. - Added basic routing. - Updated favicon to eggplant emoji.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { PlanComponent } from './plan/plan.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
