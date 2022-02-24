657a5ba7dd057afbcf8af1829e1ada5d6a68f2f2
Nathan Brinton <nab.natethegreat@gmail.com>
2022-02-24 02:27:26 -0700

    - Created mock-data JSON files. - Refactored `header.component` styling. - Created first base draft of `plan.component`. - Added some additional variables to `_appSettings.scss`; - Added bootstrap import statements to `_externalVariables.scss`. - Refactored fab font color to be the background color instead of white. - Refactored `tsconfig.json` to allow importing of JSON files. - Created `Recipe` model. - Created `generate-meals.service`.

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../models/recipe';
+import { GenerateMealsService } from '../services/generate-meals.service';
 
 @Component({
   selector: 'app-plan',
@@ -7,9 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlanComponent implements OnInit {
 
-  constructor() { }
+  breakfasts: Recipe[] = [];
+  lunches: Recipe[] = [];
+  dinners: Recipe[] = [];
 
-  ngOnInit(): void {
+  constructor(
+    private generateMealsService: GenerateMealsService
+  ) { }
+
+  ngOnInit(): void { }
+
+  onClickGenerate() {
+    this.breakfasts = this.generateMealsService.generateBreakfasts();
+    this.lunches = this.generateMealsService.generateLunches();
+    this.dinners = this.generateMealsService.generateDinners();
+  }
+
+  deleteBreakfast(index: number) {
+    this.breakfasts.splice(index, 1);
+  }
+
+  deleteLunch(index: number) {
+    this.lunches.splice(index, 1);
+  }
+
+  deleteDinner(index: number) {
+    this.dinners.splice(index, 1);
+  }
+
+  replaceBreakfast(index: number) {
+    let breakfast = this.generateMealsService.getRandomBreakfast();
+    while (breakfast.id === this.breakfasts[index].id) {
+      breakfast = this.generateMealsService.getRandomBreakfast();
+    }
+    this.breakfasts.splice(index, 1, breakfast);
+  }
+
+  replaceLunch(index: number) {
+    let lunch = this.generateMealsService.getRandomLunch();
+    while (lunch.id === this.lunches[index].id) {
+      lunch = this.generateMealsService.getRandomLunch();
+    }
+    this.lunches.splice(index, 1, lunch);
+  }
+
+  replaceDinner(index: number) {
+    let dinner = this.generateMealsService.getRandomDinner();
+    while (dinner.id === this.dinners[index].id) {
+      dinner = this.generateMealsService.getRandomDinner();
+    }
+    this.dinners.splice(index, 1, dinner);
+  }
+
+  addBreakfast() {
+    let breakfast = this.generateMealsService.getRandomBreakfast();
+    this.breakfasts.push(breakfast);
+  }
+
+  addLunch() {
+    let lunch = this.generateMealsService.getRandomLunch();
+    this.lunches.push(lunch);
+  }
+
+  addDinner() {
+    let dinner = this.generateMealsService.getRandomDinner();
+    this.dinners.push(dinner);
   }
 
 }
