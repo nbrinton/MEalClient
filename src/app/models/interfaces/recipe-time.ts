6fec2a2c21f637d1f3690c0063b300d95c5cf887
Nathan Brinton <nab.natethegreat@gmail.com>
2022-04-30 20:13:38 -0600

    - Created `RecipeTime` model. - Added `RecipeTime` properties to `view-recipe.component.ts` as well as a conversion function. - Added number formatting pipes to `view-recipe.component.html`.

---
+++ src/app/models/interfaces/recipe-time.ts
@@ -0,0 +1,5 @@
+
+export interface RecipeTime {
+  hours: number;
+  minutes: number;
+}
--- src/app/recipes/view-recipe/view-recipe.component.ts
+++ src/app/recipes/view-recipe/view-recipe.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../../services/api/api.service';
 import { Recipe } from '../../models/interfaces/recipe';
+import { RecipeTime } from '../../models/interfaces/recipe-time';
 
 @Component({
   selector: 'app-view-recipe',
@@ -24,6 +25,16 @@ export class ViewRecipeComponent implements OnInit {
     recipeSteps: []
   };
 
+  prepTime: RecipeTime = {
+    hours: 0,
+    minutes: 0
+  };
+
+  cookTime: RecipeTime = {
+    hours: 0,
+    minutes: 0
+  };
+
   constructor(
     private route: ActivatedRoute,
     private api: ApiService
@@ -35,7 +46,39 @@ export class ViewRecipeComponent implements OnInit {
 
     this.api.getRecipe(id).subscribe(result => {
       this.recipe = result;
+
+      // Convert from seconds to RecipeTime objects
+      this.prepTime = this.secondsToRecipeTime(this.recipe.prepTime);
+      this.cookTime = this.secondsToRecipeTime(this.recipe.cookTime);
+
+      // Make sure that the steps are in order
+      this.recipe.recipeSteps.sort((a, b) => {
+        if (a.order < b.order) {
+          return -1;
+        } else if (a.order > b.order) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
     }, error => console.error(error));
+  }
+
+  /**
+   * Convert a number of seconds (how the database Recipe model tracks prep and
+   * cook times) to a RecipeTime object which tracks hours and minutes.
+   * @param seconds
+   */
+  secondsToRecipeTime(seconds: number): RecipeTime {
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor(seconds % 3600 / 60);
+
+    let recipeTime: RecipeTime = {
+      hours: hours,
+      minutes: minutes
+    };
+
+    return recipeTime;
   }
 
 }
